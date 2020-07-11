e08ab9261c95f836121012e9336bc3c63097c736
Lev Shulman <lshulman@redhat.com>
2020-07-11 16:28:33 -0600

    printing input

--- artifactory/plugins/lib/src/main/java/com/citi/ocp4/jfrog/DownloadEventProcessor.java
+++ artifactory/plugins/lib/src/main/java/com/citi/ocp4/jfrog/DownloadEventProcessor.java
@@ -7,6 +7,7 @@ import org.artifactory.request.Request;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import org.artifactory.repo.RepoPath;
 
@@ -21,9 +22,11 @@ public class DownloadEventProcessor {
 	public static void printInput(Request request, RepoPath repoPath) throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		ObjectWriter writer = mapper.writer().withoutAttribute("properties");
+		
 
 		log.severe("request: " + request.toString() + " isinstanceof: " + (request instanceof Request));
-		log.severe("request json: " + mapper.writeValueAsString(request));
+		log.severe("request json: " + writer.writeValueAsString(request));
 		log.severe("repoPath: " + repoPath.toString()+ " isinstanceof: " + (repoPath instanceof RepoPath) + " class: " + repoPath.getClass());
 		log.severe("repoPath json: " + mapper.writeValueAsString(repoPath));
 
