95521c5a9bfb82b1a34e06cd022143a67db5d9af
Lev Shulman <lshulman@redhat.com>
2020-07-11 22:18:40 -0600

    adding artifactory pojos

--- artifactoryEventProcessor/src/main/java/com/ocp4/artifactory/events/api/Apis.java
+++ artifactoryEventProcessor/src/main/java/com/ocp4/artifactory/events/api/Apis.java
@@ -1,6 +1,11 @@
 package com.ocp4.artifactory.events.api;
 
+import java.util.Map;
+import java.util.logging.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,10 @@ public class Apis {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+	
+	@PostMapping("/downloadEvent")
+	public void downloadEvent(@RequestBody Map<String,Object> json) {
+		Logger.getLogger(this.getClass().getName()).info("consumed " + json);
 	}
 }
